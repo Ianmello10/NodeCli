37d200ff11e77591309e4e5b41fdde14f0c7b22e
Ianmello10 <lucasian.azul@gmail.com>
2024-09-09 15:24:31 -0300

    feat: graph added to visualize cryptocurrency prices

--- getCoinprice.js
+++ getCoinprice.js
@@ -2,32 +2,33 @@ import chalk from "chalk";
 import { createSpinner } from "nanospinner";
 const spinner = createSpinner();
 
-
 const api_key = process.env.API_KEY;
 const url = process.env.URL;
 
- const getCoinPrice = async (coin) => {
-    try {
-      spinner.start();
-      const res = await fetch(`${url}${coin}USDT`, {
-        headers: {
-          "x-rapidapi-key": api_key,
-          "x-rapidapi-host": "crypto-price-by-api-ninjas.p.rapidapi.com",
-        },
-      });
-  
-      // if response fail 
-      if (!res.ok) {
-        throw new Error("Failed to fetch data", res.status);
-      }
-  
-      const data = await res.json();
-     spinner.stop()
-      return data.price;
-    } catch (error) {
-      console.error(chalk.red(`Error fetching data ${error.message}`));
-      return null;
-    }
-  };
 
-  export {getCoinPrice,spinner}
+
+const getCoinPrice = async (coin) => {
+  try {
+    spinner.start();
+    const res = await fetch(`${url}${coin}/market_chart?vs_currency=usd&days=5&precision=5`, {
+      headers:{
+        accept: 'application/json',
+        'x-cg-demo-api-key': api_key,
+      },
+    });
+    // if response fail 
+    if (!res.ok) {
+      throw new Error("Failed to fetch data", res.status);
+    }
+
+    const data = await res.json();
+    spinner.stop()
+
+    return data.prices.map(price => price[1]); 
+  } catch (error) {
+    console.error(chalk.red(`Error fetching data ${error.message}`));
+    return null;
+  }
+};
+
+export { getCoinPrice, spinner } 
--- main.js
+++ main.js
@@ -1,41 +1,50 @@
 #!/usr/bin/env node
 import { select } from "@inquirer/prompts";
 import chalk from "chalk";
-import {getCoinPrice,spinner} from "./getCoinprice.js";
+import { getCoinPrice, spinner } from "./getCoinprice.js";
 import executeBanner from "./executeBanner.js";
+import asciichart from 'asciichart';
 
-const api_key = process.env.API_KEY;
-const url = process.env.URL;
 
 
 const main = async () => {
-   
- await executeBanner()
+
+  await executeBanner()
 
   const answer = await select({
     message: "Select your Coin",
     choices: [
       {
         name: "BTC",
-        value: "BTC",
+        value: "bitcoin",
         description: "Bitcoin price",
       },
 
       {
         name: "ETH",
-        value: "ETH",
+        value: "ethereum",
         description: "Ethereum price",
       },
     ],
   });
 
-  const price = await getCoinPrice(answer);
+  const prices = await getCoinPrice(answer);
 
-  if (price !== null) {
+  const chart = asciichart.plot(prices, {
+    height: 20,
+    colors: [asciichart.green],
+  });
+
+
+  if (prices !== null) {
     spinner.success({
-      text: `${answer.toUpperCase()} current price is: ${chalk.green.bold(`$${price}`)}`,
+      text: `${answer.toUpperCase()} current price is: ${chalk.green.bold(`$${prices[prices.length - 1]}`)}`,
     });
   }
+
+  console.log(chart);
 };
 
 main();
+
+
